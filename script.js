14a6a6a8f17fba9a89998ddffb871f22b7017d90
peterfraedrich <peterfraedrich@gmail.com>
2020-07-10 00:11:01 -0400

    init

---
+++ script.js
@@ -0,0 +1,9 @@
+
+// javascript
+console.log(
+    '¯\\_(ツ)_/¯\n',
+    'nothing to see here except probably some javascript deprecation messages\n',
+    'and stupid easter egg console.log() printouts.\n\n',
+    'seriously, dont you have something better to do?\n\n\n\n',
+    'lets go caps; fuck the penguins.'
+)
