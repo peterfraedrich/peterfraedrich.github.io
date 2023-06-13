a25687fb27643520a51d5d370775efd770f35f6f
peterfraedrich <peter.fraedrich@neutriino.com>
2023-06-13 13:11:19 -0400

    add buttons, bg, etc.

--- script.js
+++ script.js
@@ -7,4 +7,31 @@ console.log(
     'seriously, dont you have something better to do?\n\n\n\n',
     'lets go caps; fuck the penguins.'
 )
+lookup = {
+    linkedin: 'https://linkedin.com/in/peterchristianfraedrich',
+    medium: 'https://medium.com/@peterfraedrich',
+    github: 'https://github.com/github',
+    twitter: 'https://twitter.com/peter_fraedrich'
+}
 
+var windowMin = function () {
+    elems = document.getElementsByClassName('not-title')
+    for (e of elems) {
+        e.classList.add('d-none')
+    }
+}
+
+var windowMax = function () {
+    elems = document.getElementsByClassName('not-title')
+    for (e of elems) {
+        e.classList.remove('d-none')
+    }
+}
+
+var windowClose = function () {
+
+}
+
+var navigate = function (elem, dest) {
+    window.open(lookup[dest], '_blank').focus();
+}
